0146bed242a713ddc3972920ba63beaca6cd61c7
Matheus Rodrigues <matheusrf96@gmail.com>
2025-02-06 21:18:17 -0300

    First commit

---
+++ src/main.py
@@ -0,0 +1,22 @@
+
+import os
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+client = OpenAI(api_key=API_KEY, base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
